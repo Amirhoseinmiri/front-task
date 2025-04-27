c21daa1d9da098281ed6dac4f0da93db43c8cc0c
Amirhoseinmiri <amirhoseinmiri444@gmail.com>
2025-04-27 22:29:29 +0330

    add .env

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   //   baseURL: "https://api-football-v1.p.rapidapi.com/v3/", // Base URL for Football API
   baseURL: "https://v3.football.api-sports.io/",
   headers: {
-    "X-RapidAPI-Key": "b2a24f217338dff53258f40d702dc02b", // API key from .env.local
+    "X-RapidAPI-Key": process.env.API_FOOTBALL_KEY, // API key from .env.local
     "Content-Type": "application/json",
   },
 });
